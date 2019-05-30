5e2d7dd7735e7304571225c95b9f6d209867734d
JAYDA KADIR <jydkdr@gmail.com>
2019-05-30 15:21:29 +0100

    Single responsibility

--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = params['name1']
-    @player_2 = params['name2']
+    session[:player_1]  = params[:name1]
+    session[:player_2] = params[:name2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb :play
   end
 
